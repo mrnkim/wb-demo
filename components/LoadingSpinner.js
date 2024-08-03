7e79f5bbafb60a25ab56f71aff1519256786cc9c
MKim <mrn.kim07@gmail.com>
2024-08-03 16:30:26 +0900

    Update  LoadingSpinner

--- components/LoadingSpinner.js
+++ components/LoadingSpinner.js
@@ -1,38 +1,45 @@
-import clsx from 'clsx';
+import clsx from "clsx";
 
 function getSize(size) {
-	switch (size) {
-		case 'sm':
-			return 'w-4 h-4';
-		case 'md':
-			return 'w-5 h-5';
-		default:
-			throw new Error(`Invalid size prop: ${size}`);
-	}
+  switch (size) {
+    case "sm":
+      return "w-4 h-4";
+    case "md":
+      return "w-5 h-5";
+    case "lg":
+      return "w-10 h-10";
+    default:
+      throw new Error(`Invalid size prop: ${size}`);
+  }
 }
 
 function getColor(color) {
-	switch (color) {
-		case 'default':
-			return 'border-grey-500';
-		case 'primary':
-			return 'border-primary';
-		default:
-			throw new Error(`Invalid color prop: ${color}`);
-	}
+  switch (color) {
+    case "default":
+      return "border-grey-500";
+    case "primary":
+      return "border-green-500";
+    default:
+      throw new Error(`Invalid color prop: ${color}`);
+  }
 }
 
-const LoadingSpinner = ({ className, size = 'sm', color = 'default', ...props }) => (
-	<div
-		className={clsx(
-			getSize(size),
-			getColor(color),
-			'rounded-full border-2 border-solid border-t-transparent',
-			'animate-spin',
-			className
-		)}
-		{...props}
-	/>
+const LoadingSpinner = ({
+  className,
+  size = "sm",
+  color = "default",
+  ...props
+}) => (
+  <div
+    className={clsx(
+      getSize(size),
+      getColor(color),
+      "rounded-full border-2 border-solid border-t-transparent",
+      "animate-spin",
+      className
+    )}
+    {...props}
+  />
 );
 
 export default LoadingSpinner;
