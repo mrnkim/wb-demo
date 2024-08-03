9b10b3a134b8cd4c8b74fc898b2578e5e5ed9bf2
MKim <mrn.kim07@gmail.com>
2024-08-03 16:19:50 +0900

    Add styling to ErrorFallback

--- components/ErrorFallback.js
+++ components/ErrorFallback.js
@@ -1,21 +1,42 @@
-import { React } from "react";
+import React from "react";
+import { Alert } from "@mui/material";
+import clsx from "clsx";
+import Button from "./Button";
 
-/** Component to show when there is an error */
 function ErrorFallback({ error }) {
   const handleRefresh = () => {
     window.location.reload();
   };
 
   return (
-    <div role="alert">
-      <p>Something went wrong</p>
-      <div className="warningMessageWrapper">
-        <img src={"/Warning.svg"} alt="WarningIcon" className="icon"></img>
-        <div className="warningMessage">{error?.message || error?.error}</div>
-      </div>
-      <button className="resetButton" onClick={handleRefresh}>
+    <div
+      role="alert"
+      className="flex flex-col items-center justify-center min-h-[50vh] m-auto gap-3"
+    >
+      <Alert
+        className="!mb-3 bg-red-50"
+        icon={
+          <img
+            src="/error.svg"
+            alt="Error"
+            className={clsx("h-6 w-6", "-mr-1")}
+          />
+        }
+        variant="outlined"
+        severity="error"
+      >
+        <p className="text-body2 text-grey-900">Something went wrong.</p>
+        <span className="text-body2 text-grey-900">
+          {error?.message || error?.error}
+        </span>
+      </Alert>
+      <Button
+        type="button"
+        size="sm"
+        onClick={handleRefresh}
+      >
         Back to Home
-      </button>
+      </Button>
     </div>
   );
 }
